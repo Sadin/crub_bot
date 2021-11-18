ee91c0b4be3e5ee2790ef0750b6112ebcaf43e01
Zach Snyder <sadin@fedoraproject.org>
2021-11-17 19:36:23 -0500

    update dev

--- main.go
+++ main.go
@@ -124,9 +124,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			// append guild name to debug response
 			res = res + fmt.Sprintf("`%s`\n",guild_list[guild_list_item])
 		}
-
-		fmt.Println(res)
-
+		// send res to user
 		s.ChannelMessageSend(m.ChannelID, res)
 	}
 
